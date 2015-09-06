a32575230994214d8fa35e016f42269908523ea3
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-06 17:18:24 +0530

    Fetch info

--- fetch.py
+++ fetch.py
@@ -1,10 +1,14 @@
 import requests
 import urllib
-import json
+import arrow
 
 def search(term):
     r = requests.get(itunes_search_url + urllib.quote(term)).json()
-    print r['results'][0]['trackName']
+    results = r['results']
+    print results[0]['trackName']
+    print results[0]['artistName']
+    released = arrow.get(results[0]['releaseDate'])
+    print released.format('MMMM, YYYY')
 
 name = raw_input('Please enter the name of the song(e.g. "Rocket Man Elton John): ')
 itunes_search_url = "https://itunes.apple.com/search?term="
