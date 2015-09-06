ee27ff0b005072a1460cbdadac29ed4d8ebf5b71
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-06 20:24:08 +0530

    Display retrieved info

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ $("#songForm").submit(function (event) {
 
 
 function itunesFetch(data) {
- got('https://itunes.apple.com/search?term=' + data, {json:true}, function (err, data, res) {
-    document.write(data.results[0].artistName);
-});
+    got('https://itunes.apple.com/search?term=' + data, {json: true}, function (err, data, res) {
+        $('#artwork').attr('src', data.results[0].artworkUrl100);
+        $('#song').text((data.results[0].trackName));
+        $('#album').text((data.results[0].collectionName));
+        $('#artist').text((data.results[0].artistName));
+        $('#released').text((data.results[0].releaseDate));
+    });
 }
-
