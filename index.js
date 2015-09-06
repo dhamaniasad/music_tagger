b5552b8b98cd58ec9e0211544f97205a8d05561a
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-06 19:32:38 +0530

    Add form

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var got = require('got');
+window.$ = window.jQuery = require('./jquery.min.js');
 
-got('https://itunes.apple.com/search?term=rocket%20man', function (err, data, res) {
-    document.write(data);
+$("#songForm").submit(function (event) {
+    alert("Handler for .submit() called.");
+    event.preventDefault();
 });
+
+
+//got('https://itunes.apple.com/search?term=rocket%20man', function (err, data, res) {
+//    document.write(data);
+//});
