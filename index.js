541cc2b9e463f75bc9380e2e0b972ba1b8ea4381
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-06 21:37:57 +0530

    Add howler.js for audio playback

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ $("#songForm").submit(function (event) {
 
 function itunesFetch(data) {
     got('https://itunes.apple.com/search?term=' + data, {json: true}, function (err, data, res) {
-        $('#artwork').attr('src', data.results[0].artworkUrl100);
+        var artworkUrl = data.results[0].artworkUrl100.replace('100x100', '150x150');
+        $('#artwork').attr('src', artworkUrl);
         $('#song').text((data.results[0].trackName));
+        $('#genre').text((data.results[0].primaryGenreName));
         $('#album').text((data.results[0].collectionName));
         $('#artist').text((data.results[0].artistName));
         $('#released').text((data.results[0].releaseDate));
